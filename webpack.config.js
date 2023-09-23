43ab1c6d615e856fbd11251712613aa7a90a9b9c
Aleksey <1aleksey.mazur@gmail.com>
2023-09-24 00:08:56 +0300

    dev and build scripts added in package.json; work with html: html-webpack-plugin, clean-webpack-plugin added and configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -10,4 +12,10 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
