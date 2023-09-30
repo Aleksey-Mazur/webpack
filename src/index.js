4bfefb6cd8d2c6a2e4c015ebb323456fab352ec7
Aleksey <1aleksey.mazur@gmail.com>
2023-09-30 20:46:47 +0300

    extensions and aliases added to config; optimization configured in webpack.config with jquery as an example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import Post from './Post';
-import json from './assets/json';
-import WebpackLogo from './assets/webpack-logo.png';
+import * as $ from 'jquery';
+import Post from '@models/Post';
+// import json from './assets/json';
+import WebpackLogo from '@/assets/webpack-logo';
 import './styles/styles.css';
 
 const post = new Post('Webpack Post Title', WebpackLogo);
 
-console.log('Post to String:', post.toString());
+$('pre').html(post.toString());
 
-console.log('JSON:', json);
+// console.log('Post to String:', post.toString());
+// console.log(post.uppercaseTitle);
+
+// console.log('JSON:', json);
